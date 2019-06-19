ebdfb558e8b7d4d69bae49ae322b35a3717ce603
Lupo Solitario <duyluong1994@gmail.com>
2019-06-19 16:20:25 +0700

    snap tool

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ class Settings {
     public TABLE: string | undefined;
     public EXCLUDE_ACCOUNTS: string[] = [];
     public DFUSE_API_KEY = "eyJhbGciOiJLTVNFUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NTI3NjM4MzQsImp0aSI6IjU4ZmIzZDY3LTI4NmItNDEyZC05NWI5LWQ1YWYyMjA5NTQ0MyIsImlhdCI6MTU1MDE3MTgzNCwiaXNzIjoiZGZ1c2UuaW8iLCJzdWIiOiJDaVFBNmNieWV6NUxNUGNSQzZrZm1GbVhqWTNGSzlOR21LK3h6QVNrSXZBZXhCeXZBeGtTUVFBL0NMUnRCUVlvazh6ZFZkb2VVejJlZ1FmSU15TnR0Z3QyVExPdWdIVVUrUjhkdzVka1JRSjNjWVZEaFdyakVwZ0VlSDd5clVYOGZIUHlFVm1scENvZSIsInRpZXIiOiJiZXRhLXYxIiwidiI6MX0.s1K7pt-8oxRswnMimZO2TunrlMByL-gcGG0v2UNQ-FF53qldjLjkK1--NgUq_W3Lk9AQmjYMQdooGmRDDeNEng";
-    public EOSIO_ENDPOINT= "https://eos.greymass.com";
+    public EOSIO_ENDPOINT= "https://dsp.airdropsdac.com";
     public DFUSE_ENDPOINT = "https://mainnet.eos.dfuse.io";
     public MIN_BALANCE = 0;
     public CSV_HEADERS = false;
